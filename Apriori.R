131b315cfd7cfe3f619dcbe92d6ddac688c56b26
peyam ekenel <150328297+peyamekenel@users.noreply.github.com>
2024-05-28 15:26:30 +0300

    first database definition deleted

--- Apriori.R
+++ Apriori.R
@@ -1,8 +1,6 @@
 #install.packages('arules')
 library(arules)
 
-dataset= read.csv('Market_Basket_Optimisation.csv',header = FALSE)
-
 dataset= read.transactions('Market_Basket_Optimisation.csv',sep = ',',rm.duplicates = TRUE)
 summary(dataset)
 itemFrequencyPlot(dataset,topN=10)
